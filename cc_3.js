2d4a5418e9aded62a3ce4bb07250c601c02ff7e7
rileygoerg <rgoerg@usf.edu>
2024-05-20 23:58:36 -0400

    Updated comments for function clarity

--- cc_3.js
+++ cc_3.js
@@ -5,12 +5,12 @@ class Car {
         this.make = make;
         this.speed = speed;
     }
-    // Part 2: Accelerate Function
+    // Part 2: Accelerate Function. Adds 10 km/h to current car speed
     accelerate(){
         this.speed += 10;
         console.log("The ", this.make, "accelerated. Current speed: ", this.speed, "km/h");
     }
-    // Part 3: Brake Function
+    // Part 3: Brake Function. Removes 5 km/h from current car speed
     brake(){
         this.speed -= 5;
         console.log("The ", this.make, "braked. Current speed: ", this.speed, "km/h");
