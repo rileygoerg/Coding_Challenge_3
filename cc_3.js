2787e10f446054951c0ffdc165a5c2c48072431b
rileygoerg <rgoerg@usf.edu>
2024-05-20 23:56:02 -0400

    Test accelerate and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -16,3 +16,27 @@ class Car {
         console.log("The ", this.make, "braked. Current speed: ", this.speed, "km/h");
     }
 }
+// Creating 2 cars from dataset
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+// Initializing Car 1
+console.log(car1.make, "starting at", car1.speed, "km/h.");
+// Testing Accelerate and Brake Methods for Car 1
+car1.accelerate()
+car1.accelerate()
+car1.brake()
+car1.accelerate()
+car1.brake()
+// Logging Car 1's new speed
+console.log(car1.make, "accelerated 3 times (+30km/h) and braked 2 times (-10km/h). New speed: ", car1.speed, "km/h.");
+// Initializing Car 2
+console.log(car2.make, "starting at", car2.speed, "km/h.");
+// Testing Accelerate and Brake Methods for Car 2
+car2.brake()
+car2.brake()
+car2.accelerate()
+car2.brake()
+car2.accelerate()
+//Logging Car 2's New Speed
+console.log(car2.make, "accelerated 2 times (+20km/h) and braked 3 times (-15km/h). New speed:", car2.speed, "km/h.");
